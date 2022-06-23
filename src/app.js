9c37d4e2e91912bf611cb7aacefcd1199f1eec6c
Lucas Lafèré <laferelucas@gmail.com>
2022-06-23 17:56:09 -0300

    added participant name post

--- src/app.js
+++ src/app.js
@@ -11,8 +11,27 @@ app.use(json());
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
+
 let db;
 
+//incompleto
+app.post('/participants', async (req, res) => {
+    const username = req.body.name;
+    try {
+        mongoClient.connect()
+        const databaseUol = mongoClient.db("api-uol");
+        const collectionParticipants = databaseUol.collection("participants");
+        if (!username) {
+            res.status(422).send("Preencha o campo de usuário")
+            return
+        }
+        await collectionParticipants.insertOne({name: username})
+
+
+    } catch (error) {
+
+    }
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor rodando na porta ${process.env.PORT}`)
