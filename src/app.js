52621f3c8a2a369f5d728f847125956e72d472e1
Lucas Lafèré <laferelucas@gmail.com>
2022-06-24 17:30:55 -0300

    added get/messages route, with full functionality and privacy stuff

--- src/app.js
+++ src/app.js
@@ -16,6 +16,12 @@ let db;
 const participantSchema = joi.object({
     name: joi.string().required()
 });
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('private-message', 'message').required()
+});
+
 
 app.post('/participants', async (req, res) => {
     const username = req.body;
@@ -38,7 +44,7 @@ app.post('/participants', async (req, res) => {
         }
 
         await collectionParticipants.insertOne({name: username.name, lastStatus: Date.now()});
-        await collectionRoomMessage.insertOne({from: username.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')})
+        await collectionRoomMessage.insertOne({from: username.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')});
         res.sendStatus(201);
         return;
 
@@ -65,8 +71,33 @@ app.get('/participants', async (req, res) => {
 })
 
 app.post('/messages', async (req, res) => {
+    const message = req.body;
+    const from = req.headers.user;
+    const validation = messageSchema.validate(message, { abortEarly: false });
+    if (validation.error) {
+        console.log(validation.error.details);
+        res.status(422).send(validation.error.details);
+        return
+    }
+    if (!req.headers.user) {
+        res.sendStatus(422);
+        return;
+    }
     try {
         await mongoClient.connect();
+        db = mongoClient.db("api-uol");
+        const collectionRoomMessage = db.collection("roomMessage");
+        const collectionParticipants = db.collection("participants");
+        await console.log(from);
+        const checkExistingUser = await collectionParticipants.findOne({name: from});
+
+        if (!checkExistingUser) {
+            res.status(422).send(`O nome ${from} não existe`);
+            return;
+        }
+        await collectionRoomMessage.insertOne({from: from, to: message.to, text: message.text, type: message.type, time: dayjs().format('HH:mm:ss')});
+        res.status(201).send("funcionou");
+        return;
 
     } catch (error) {
         res.status(500).send(error);
@@ -75,8 +106,28 @@ app.post('/messages', async (req, res) => {
 })
 
 app.get('/messages', async (req, res) => {
+    let messages;
+    const limit = parseInt(req.query.limit);
+    const user = req.headers.user;
+
     try {
         await mongoClient.connect();
+        db = mongoClient.db("api-uol");
+        const collectionRoomMessage = db.collection("roomMessage");
+        
+    if (!req.headers.user) {
+        res.sendStatus(422);
+        return;
+    }
+    if (!req.query.limit) {
+        messages = await collectionRoomMessage.find({  $or : [ {"from" : user}, {"to" : user}, {"type" : "message"}, {"type" : "status"} ] } ).toArray();
+        res.status(200).send(messages);
+        return;
+    }
+        messages = await collectionRoomMessage.find({  $or : [ {"from" : user}, {"to" : user}, {"type" : "message"} ] } ).limit(limit).toArray();
+        res.status(200).send(messages);
+        return;
+
 
     } catch (error) {
         res.status(500).send(error);
