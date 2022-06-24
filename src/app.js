f88e982c7cba38721d1bcfba12cb0836e972af35
Lucas Lafèré <laferelucas@gmail.com>
2022-06-24 15:35:35 -0300

    added enter status to collection, added last status

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { MongoClient } from 'mongodb';
 import express, { json } from 'express';
 import cors from 'cors';
 import joi from 'joi';
+import dayjs from 'dayjs';
 
 dotenv.config();
 const app = express();
@@ -12,26 +13,37 @@ app.use(json());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
+const participantSchema = joi.object({
+    name: joi.string().required()
+});
 
 app.post('/participants', async (req, res) => {
-    const username = req.body.name;
+    const username = req.body;
+    const validation = participantSchema.validate(username, { abortEarly: false });  
+    if (validation.error) {
+        console.log(validation.error.details)
+        res.status(422).send(validation.error.details);
+        return;     
+    }
     try {
         await mongoClient.connect();
         db = mongoClient.db("api-uol");
         const collectionParticipants = db.collection("participants");
-        if (!username) {
-            res.status(422).send("Preencha o campo de usuário");
-            mongoClient.close();
+        const collectionRoomStatusMessage = db.collection("roomStatusMessage");
+        const checkSameUser = await collectionParticipants.findOne(username);
+
+        if (checkSameUser) {
+            res.status(409).send(`O nome ${username.name} já existe`);
             return;
         }
-        await collectionParticipants.insertOne({ name: username });
-        res.sendStatus(200);
-        mongoClient.close();
+
+        await collectionParticipants.insertOne({name: username.name, lastStatus: Date.now()});
+        await collectionRoomStatusMessage.insertOne({from: username.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')})
+        res.sendStatus(201);
         return;
 
     } catch (error) {
         res.status(500).send(error);
-        mongoClient.close();
         return;
     }
 })
@@ -44,12 +56,10 @@ app.get('/participants', async (req, res) => {
         const collectionParticipants = db.collection("participants");
         participants = await collectionParticipants.find().toArray();
         res.status(200).send(participants);
-        mongoClient.close();
         return;
 
     } catch (error) {
         res.status(500).send(error);
-        mongoClient.close();
         return;
     }
 })
@@ -60,7 +70,6 @@ app.post('/messages', async (req, res) => {
 
     } catch (error) {
         res.status(500).send(error);
-        mongoClient.close();
         return;
     }
 })
@@ -71,7 +80,6 @@ app.get('/messages', async (req, res) => {
 
     } catch (error) {
         res.status(500).send(error);
-        mongoClient.close();
         return;
     }
 })
@@ -82,7 +90,6 @@ app.post('/status', async (req, res) => {
 
     } catch (error) {
         res.status(500).send(error);
-        mongoClient.close();
         return;
     }
 })
